fbc37a07bfcea4767d43ed3277952bcf0f40dc4f
German Ravarotto <g44ravarotto@gmail.com>
2023-11-29 12:28:03 -0300

    responsive

--- src/components/CardData.jsx
+++ src/components/CardData.jsx
@@ -1,29 +1,25 @@
-
 const CardWeather = ({ data }) => {
     console.log(data)
     return (
-        <div className="w-full h-full ">
-            <div className="w-full h-1/2 flex justify-around    ">
-                <div className="flex flex-col items-center ">
-                    <h3 className="text-[4.5rem] font-bold ">{data.name} <span className="text-[3rem] font-semibold ">{data.sys.country}</span></h3>
-                    <img className="w-[10rem]" src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="Imagen" />
-                    <p className="text-[1.5rem] font-semibold ">{data.weather[0].description}</p>
-                </div>
-                <div className="flex flex-col justify-center items-center ">
-                    <h2 className="text-[2rem] font-semibold ">Temperatura</h2>
-                    <h3 className="text-[12rem] font-bold ">{Math.round(data.main.temp)}°C</h3>
-                </div>
-            </div>
-            <div className="w-full h-1/2 ">
-                <h3 className="mx-3 text-[3rem] font-semibold ">Sensacion termica: <span className="text-[4rem] font-bold ">{Math.round(data.main.feels_like)}°C</span></h3>
-                <h3 className="mx-3  text-[3rem] font-semibold ">Temperatura Min/Max: <span className="text-[4rem] font-bold ">{Math.round(data.main.temp_min)}°C</span> / <span className="text-[4rem] font-bold ">{Math.round(data.main.temp_max)}°C</span></h3>
-                <h3 className="mx-3 text-[3rem] font-semibold ">Humedad: <span className="text-[4rem] font-bold ">{data.main.humidity}%</span></h3>
-                <h3 className="mx-3 text-[3rem] font-semibold ">Presion: <span className="text-[4rem] font-bold ">{data.main.pressure}hPa</span></h3>
-
-
-
-            </div>
-        </div>
+        <main className="w-full h-full ">
+            <section className="w-full h-1/2 flex flex-col md:flex-row lg:justify-around justify-center ">
+                <article className="flex flex-col items-center ">
+                    <h3 className="lg:text-[4.5rem] text-[2rem] font-bold ">{data.name} <span className="lg:text-[3rem] text-[1rem] font-semibold ">{data.sys.country}</span></h3>
+                    <img className="w-[5rem] lg:w-[10rem]" src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="Imagen" />
+                    <p className="text-[1rem] lg:text-[1.5rem] font-semibold ">{data.weather[0].description}</p>
+                </article>
+                <article className="flex flex-col justify-center items-center ">
+                    <h2 className=" hidden lg:block lg:text-[2rem] font-semibold ">Temperatura</h2>
+                    <h3 className="text-[4rem] lg:text-[12rem] font-bold ">{Math.round(data.main.temp)}°C</h3>
+                </article>
+            </section>
+            <ul className="list-disc pl-4 lg:mx-3  w-full h-1/2  flex flex-col justify-center items-start">
+                <li className="mx-2 lg:text-[3rem] font-semibold ">Sensacion termica: <span className="text-[1.5rem] lg:text-[4rem] font-bold ">{Math.round(data.main.feels_like)}°C</span></li>
+                <li className="mx-2 lg:text-[3rem] text-[1rem] font-semibold ">Temperatura Min/Max: <span className="text-[1.5rem] lg:text-[4rem] font-bold ">{Math.round(data.main.temp_min)}°C</span> / <span className="text-[1.5rem] lg:text-[4rem] font-bold ">{Math.round(data.main.temp_max)}°C</span></li>
+                <li className="mx-2  lg:text-[3rem] text-[1rem] font-semibold ">Humedad: <span className="text-[1.5rem] lg:text-[4rem] font-bold ">{data.main.humidity}%</span></li>
+                <li className="mx-2 lg:text-[3rem] text-[1rem] font-semibold ">Presion: <span className="text-[1.5rem] lg:text-[4rem] font-bold ">{data.main.pressure} hPa</span></li>
+            </ul>
+        </main>
     )
 }
 export default CardWeather
